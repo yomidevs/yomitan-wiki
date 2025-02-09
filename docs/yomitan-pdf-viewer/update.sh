1dd1085dcca886ac4cad253e4d52d3f2ee18b1a9
kuuuube <hexagonisalie@gmail.com>
2025-02-09 18:07:07 -0500

    Update yomitan pdf viewer update script

--- docs/yomitan-pdf-viewer/update.sh
+++ docs/yomitan-pdf-viewer/update.sh
@@ -5,6 +5,9 @@ wget "https://github.com/mozilla/pdf.js/releases/download/v$pdfjs_version/pdfjs-
 unzip -o "pdfjs-$pdfjs_version-dist.zip" "web/*" -d "."
 unzip -o "pdfjs-$pdfjs_version-dist.zip" "build/*" -d "."
 mv "./web/viewer.html" "./web/index.html"
-cp "yomitan-pdf-viewer.pdf" "./web/compressed.tracemonkey-pldi-09.pdf"
+
+cp "yomitan-pdf-viewer.pdf" "./web/yomitan-pdf-viewer.pdf"
+sed -i -e 's/"compressed.tracemonkey-pldi-09.pdf"/"yomitan-pdf-viewer.pdf"/g' ./web/viewer.mjs
+rm ./web/compressed.tracemonkey-pldi-09.pdf
 
 rm "pdfjs-$pdfjs_version-dist.zip"
